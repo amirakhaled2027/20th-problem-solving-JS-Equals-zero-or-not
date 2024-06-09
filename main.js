5c663608541b5f1e949d61cfe9b848e38f85d569
Amira Khaled <155392660+amirakhaled2027@users.noreply.github.com>
2024-06-09 22:29:14 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+//create a function that return true if the outcome is true either it's false
+function equalZero(num) {
+    if (num == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  console.log(equalZero(7));
